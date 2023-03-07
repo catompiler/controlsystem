e2477735f90bd9e1a21f44c8fb8c7562f0620028
artem.lab <artem.lab@gmail.com>
2023-03-07 13:59:57 +0500

    iqmath header

---
+++ iqmath/iqmath.h
@@ -0,0 +1,13 @@
+#ifndef IQ_MATH_H
+#define IQ_MATH_H
+
+#include "iq24.h"
+#include "iq24_math.h"
+#include "iq24_cordic.h"
+#include "iq15.h"
+#include "iq15_math.h"
+#include "iq15_cordic.h"
+#include "q15.h"
+#include "q15_math.h"
+
+#endif /* IQ_MATH_H */
