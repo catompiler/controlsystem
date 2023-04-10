61fc9c0b1bef57ae5d09ee914557b5a88e00b400
artem.lab <artem.lab@gmail.com>
2023-04-10 15:42:46 +0500

    main writing data log to csv file

--- main.c
+++ main.c
@@ -1,133 +1,95 @@
+#include "modules/modules.h"
 #include <stdio.h>
 #include <stddef.h>
+#include "reg/regs.h"
 
-#include <math.h>
+//#include <sys/time.h>
+//#include <stdio.h>
 
-#include "iqmath/iq.h"
-#include "iqmath/iq24.h"
-#include "iqmath/iq24_math.h"
-#include "iqmath/iq15.h"
-#include "iqmath/iq15_math.h"
-#include "iqmath/iq24_cordic.h"
-#include "iqmath/q15.h"
-#include "iqmath/q15_math.h"
 
 #ifndef __arm__
+#define RUN_TESTS 0
+#endif
+
+#if defined(RUN_TESTS) && RUN_TESTS == 1
 #include "test_main.h"
 #endif
 
-/*
-
-#include <signal.h>
-#include <time.h>
-
-
-#define COUNTER_TOP 1
-timer_t timerid;
-int counter = 0;
-
-void timer_notify_function(union sigval arg)
+static void write_dlog_to_file(void)
 {
-    (void) arg;
-    printf("%d\n", ++counter);
-    fflush(stdout);
+    FILE* f = fopen("dlog.csv", "w");
+
+    uint32_t get_index = dlog.r_get_index;
+    uint32_t i, j;
+    for(i = 0; i < dlog.r_count; i ++){
+        for(j = 0; j < DATA_LOG_CH_COUNT; j ++){
+            fprintf(f, "%d", dlog.r_ch[j].data[get_index]);
+            if(j != (DATA_LOG_CH_COUNT - 1)){
+                fprintf(f, ", ");
+            }else{
+                fprintf(f, "\n");
+            }
+        }
+        DATA_LOG_NEXT_INDEX(get_index);
+    }
+
+    fclose(f);
 }
-
-int init_timer(void)
-{
-    int res = 0;
-
-    struct sigevent sigev;
-
-    sigev.sigev_notify = SIGEV_THREAD;
-    sigev.sigev_signo = 0;
-    sigev.sigev_notify_attributes = 0;
-    sigev.sigev_notify_function = timer_notify_function;
-    sigev.sigev_value.sival_ptr = NULL;
-
-    res = timer_create(CLOCK_REALTIME, &sigev, &timerid);
-    if(res != 0) return res;
-
-    struct itimerspec t;
-    t.it_value.tv_sec = 0;
-    t.it_value.tv_nsec = 100000000;
-    t.it_interval.tv_sec = 0;
-    t.it_interval.tv_nsec = 100000000;
-
-    res = timer_settime(timerid, 0, &t, NULL);
-    if(res != 0) return res;
-
-    return res;
-}
-
-int deinit_timer(void)
-{
-    int res = 0;
-
-    res = timer_delete(timerid);
-
-    return res;
-}
-*/
-#ifdef __arm__
-int printf(const char* __restrict, ...)
-{
-    return 0;
-}
-#endif
-
 
 
 int main(void)
 {
-/*
-    int res = 0;
+    //loadsettings();
 
-    res = init_timer();
-    if(res != 0){
-        printf("Error init timer!\n");
-        return res;
+    dlog.p_ch[0].reg_id = REG_ID_ADC_UA;
+    dlog.p_ch[0].enabled = 1;
+    dlog.p_ch[1].reg_id = REG_ID_ADC_UB;
+    dlog.p_ch[1].enabled = 1;
+    dlog.p_ch[2].reg_id = REG_ID_ADC_UC;
+    dlog.p_ch[2].enabled = 1;
+    dlog.control = CONTROL_ENABLE;
+
+    INIT(sys);
+
+    if(sys.status & SYS_MAIN_STATUS_ERROR){
+        printf("Error init main system!\n");
+        DEINIT(sys);
+        return 0;
     }
 
-    while(counter < COUNTER_TOP);
+    for(;;){
+        IDLE(sys);
 
-    deinit_timer();
-*/
-#ifndef __arm__
+        if(adc_tim.out_counter >= DATA_LOG_CH_LEN) break;
+
+//        struct timespec t;
+//        clock_gettime(CLOCK_MONOTONIC, &t);
+//        printf("%ld s %ld ns\n", t.tv_sec, t.tv_nsec);
+    }
+
+    dlog.control = CONTROL_NONE;
+
+    DEINIT(sys);
+
+    write_dlog_to_file();
+
+    printf("done\n");
+
+//    struct timespec t;
+//    clock_getres(CLOCK_MONOTONIC, &t);
+//    printf("Timer res: %ld sec %ld nsec\n", t.tv_sec, t.tv_nsec);
+
+
+//    reg_t* r = regs_find(REG_ID_ADC_UA);
+//    if(r != NULL){
+//        printf("reg id %d, search id %d\n", r->id, REG_ID_ADC_UA);
+//        printf("reg data: 0x%x\n", reg_value(r, int));
+//    }
+
+    //printf("0x%x 0x%x\n", conf.r_inv_U_nom, conf.r_inv_I_nom);
+
+#if defined(RUN_TESTS) && RUN_TESTS == 1
     test_main();
-#endif
-
-    /*iq24_t sin_val = iq24_sin(IQ24_PI/4);
-    printf("sin(pi/4): 0x%x\n", sin_val);*/
-
-    /*iq24_t sqrt_val = iq24_sqrt(IQ24I(2));
-    printf("sqrt(2): 0x%x\n", sqrt_val);*/
-
-    /*iq15_t sqrt_val = iq15_sqrt(IQ15I(2));
-    printf("sqrt(2): 0x%x\n", sqrt_val);*/
-
-    /*iq24_t x, y, angle, hyp;
-    x = IQ24(0);
-    y = IQ24(1);
-    iq24_cordic_atan2_hyp_pu(x, y, &angle, &hyp);
-    printf("atan 0x%x hyp 0x%x\n", angle, hyp);
-
-    angle = IQ24(0.0);
-    iq24_cordic_sincos_pu(angle, &y, &x);
-    printf("sin 0x%x cos 0x%x\n", y, x);*/
-
-    //print_iq15_cordic_atan2_pu();
-
-    /*q15_t sin_val = q15_sin_pu(Q15(0.125));
-    printf("sin(pi/4): 0x%x\n", sin_val);*/
-
-    /*q15_t sin_val = q15_sin_pu(0xdfa4);
-    printf("sin(pi/4): 0x%x\n", sin_val);*/
-
-#ifndef __arm__
-
-    //print_iq24_sqrt();
-
 #endif
 
     return 0;
