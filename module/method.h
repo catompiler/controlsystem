ea8dd0987099dbeaf3afb436c9ef6dd5c931f497
artem.lab <artem.lab@gmail.com>
2023-04-10 15:10:28 +0500

    module methods ret type

--- module/method.h
+++ module/method.h
@@ -35,8 +35,9 @@
 /**
  * Объявление метода в структуре.
  * @param MOD_TYPE Тип модуля для аргумента this функции.
+ * @param NAME Имя метода.
  */
-#define METHOD(MOD_TYPE, NAME, ...) void (*NAME)(MOD_TYPE*, ##__VA_ARGS__)
+#define METHOD(MOD_TYPE, NAME, RET, ...) RET (*NAME)(MOD_TYPE*, ##__VA_ARGS__)
 /**
  * Вспомогательный макрос - формирует имя функции метода модуля.
  * @param MOD_NAME Имя модуля.
@@ -56,7 +57,7 @@
  * @param MOD_TYPE Тип модуля для аргумента this функции.
  * @param NAME Имя метода.
  */
-#define METHOD_PROTO_S(MOD_NAME, MOD_TYPE, NAME, ...) void METHOD_FUNC_NAME(MOD_NAME, NAME)(MOD_TYPE*, ##__VA_ARGS__)
+#define METHOD_PROTO_S(MOD_NAME, MOD_TYPE, NAME, RET, ...) RET METHOD_FUNC_NAME(MOD_NAME, NAME)(MOD_TYPE*, ##__VA_ARGS__)
 /**
  * Объявляет прототип метода модуля,
  * для указанием типа структуры модуля
@@ -64,7 +65,7 @@
  * @param MOD_NAME Имя модуля.
  * @param NAME Имя метода.
  */
-#define METHOD_PROTO(MOD_NAME, NAME, ...) METHOD_PROTO_S(MOD_NAME, MOD_NAME, NAME, ##__VA_ARGS__)
+#define METHOD_PROTO(MOD_NAME, NAME, RET, ...) METHOD_PROTO_S(MOD_NAME, MOD_NAME, NAME, RET, ##__VA_ARGS__)
 /**
  * Объявляет реализацию метода модуля
  * с указанием типа структуры модуля.
@@ -73,7 +74,7 @@
  * @param THIS Имя аргумента this.
  * @param NAME Имя метода.
  */
-#define METHOD_IMPL_S(MOD_NAME, MOD_TYPE, THIS, NAME, ...) void METHOD_FUNC_NAME(MOD_NAME, NAME)(MOD_TYPE* THIS, ##__VA_ARGS__)
+#define METHOD_IMPL_S(MOD_NAME, MOD_TYPE, THIS, NAME, RET, ...) RET METHOD_FUNC_NAME(MOD_NAME, NAME)(MOD_TYPE* THIS, ##__VA_ARGS__)
 /**
  * Объявляет реализацию метода модуля,
  * для указанием типа структуры модуля
@@ -82,88 +83,96 @@
  * @param THIS Имя аргумента this.
  * @param NAME Имя метода.
  */
-#define METHOD_IMPL(MOD_NAME, THIS, NAME, ...) METHOD_IMPL_S(MOD_NAME, MOD_NAME, THIS, NAME, ##__VA_ARGS__)
+#define METHOD_IMPL(MOD_NAME, THIS, NAME, RET, ...) METHOD_IMPL_S(MOD_NAME, MOD_NAME, THIS, NAME, RET, ##__VA_ARGS__)
 /**
  * Вызывает метод модуля.
  * @param MOD Переменная модуля.
  * @param NAME Имя модуля.
  */
-#define CALL(MOD, NAME, ...) do { MOD.NAME(&MOD, ##__VA_ARGS__); } while(0)
+#define CALL(MOD, NAME, ...) MOD.NAME(&MOD, ##__VA_ARGS__)
 // /**
 //  * Вызывает метод модуля по указателю.
 //  * @param MOD Переменная модуля.
 //  * @param NAME Имя модуля.
 //  */
-// #define CALL_PTR(MOD_PTR, NAME, ...) do { MOD_PTR->NAME(MOD_PTR, ##__VA_ARGS__); } while(0)
+// #define CALL_PTR(MOD_PTR, NAME, ...) MOD_PTR->NAME(MOD_PTR, ##__VA_ARGS__)
 
 
 // Метод init().
 // Инициализацию модуля.
 #define METHOD_INIT_M_NAME init
-#define METHOD_INIT(MOD_TYPE)                        METHOD(MOD_TYPE, METHOD_INIT_M_NAME)
+#define METHOD_INIT_RET void
+#define METHOD_INIT(MOD_TYPE)                        METHOD(MOD_TYPE, METHOD_INIT_M_NAME, METHOD_INIT_RET)
 #define METHOD_INIT_PTR(MOD_NAME)                    METHOD_PTR(MOD_NAME, METHOD_INIT_M_NAME)
-#define METHOD_INIT_PROTO(MOD_NAME)                  METHOD_PROTO(MOD_NAME, METHOD_INIT_M_NAME)
-#define METHOD_INIT_IMPL(MOD_NAME, THIS)             METHOD_IMPL(MOD_NAME, THIS, METHOD_INIT_M_NAME)
+#define METHOD_INIT_PROTO(MOD_NAME)                  METHOD_PROTO(MOD_NAME, METHOD_INIT_M_NAME, METHOD_INIT_RET)
+#define METHOD_INIT_IMPL(MOD_NAME, THIS)             METHOD_IMPL(MOD_NAME, THIS, METHOD_INIT_M_NAME, METHOD_INIT_RET)
 #define INIT(MOD)                                     CALL(MOD, METHOD_INIT_M_NAME)
 
 // Метод deinit().
 // Деинициализация модуля.
 #define METHOD_DEINIT_M_NAME deinit
-#define METHOD_DEINIT(MOD_TYPE)                        METHOD(MOD_TYPE, METHOD_DEINIT_M_NAME)
+#define METHOD_DEINIT_RET void
+#define METHOD_DEINIT(MOD_TYPE)                        METHOD(MOD_TYPE, METHOD_DEINIT_M_NAME, METHOD_DEINIT_RET)
 #define METHOD_DEINIT_PTR(MOD_NAME)                    METHOD_PTR(MOD_NAME, METHOD_DEINIT_M_NAME)
-#define METHOD_DEINIT_PROTO(MOD_NAME)                  METHOD_PROTO(MOD_NAME, METHOD_DEINIT_M_NAME)
-#define METHOD_DEINIT_IMPL(MOD_NAME, THIS)             METHOD_IMPL(MOD_NAME, THIS, METHOD_DEINIT_M_NAME)
+#define METHOD_DEINIT_PROTO(MOD_NAME)                  METHOD_PROTO(MOD_NAME, METHOD_DEINIT_M_NAME, METHOD_DEINIT_RET)
+#define METHOD_DEINIT_IMPL(MOD_NAME, THIS)             METHOD_IMPL(MOD_NAME, THIS, METHOD_DEINIT_M_NAME, METHOD_DEINIT_RET)
 #define DEINIT(MOD)                                     CALL(MOD, METHOD_DEINIT_M_NAME)
 
 // Метод calc().
 // Основная итерация работы, расчёта модуля.
 #define METHOD_CALC_M_NAME calc
-#define METHOD_CALC(MOD_TYPE)                        METHOD(MOD_TYPE, METHOD_CALC_M_NAME)
+#define METHOD_CALC_RET void
+#define METHOD_CALC(MOD_TYPE)                        METHOD(MOD_TYPE, METHOD_CALC_M_NAME, METHOD_CALC_RET)
 #define METHOD_CALC_PTR(MOD_NAME)                    METHOD_PTR(MOD_NAME, METHOD_CALC_M_NAME)
-#define METHOD_CALC_PROTO(MOD_NAME)                  METHOD_PROTO(MOD_NAME, METHOD_CALC_M_NAME)
-#define METHOD_CALC_IMPL(MOD_NAME, THIS)             METHOD_IMPL(MOD_NAME, THIS, METHOD_CALC_M_NAME)
+#define METHOD_CALC_PROTO(MOD_NAME)                  METHOD_PROTO(MOD_NAME, METHOD_CALC_M_NAME, METHOD_CALC_RET)
+#define METHOD_CALC_IMPL(MOD_NAME, THIS)             METHOD_IMPL(MOD_NAME, THIS, METHOD_CALC_M_NAME, METHOD_CALC_RET)
 #define CALC(MOD)                                     CALL(MOD, METHOD_CALC_M_NAME)
 
 // Метод update().
 //#define METHOD_UPDATE_M_NAME update
-//#define METHOD_UPDATE(MOD_TYPE)                        METHOD(MOD_TYPE, METHOD_UPDATE_M_NAME)
+//#define METHOD_UPDATE_RET void
+//#define METHOD_UPDATE(MOD_TYPE)                        METHOD(MOD_TYPE, METHOD_UPDATE_M_NAME, METHOD_UPDATE_RET)
 //#define METHOD_UPDATE_PTR(MOD_NAME)                    METHOD_PTR(MOD_NAME, METHOD_UPDATE_M_NAME)
-//#define METHOD_UPDATE_PROTO(MOD_NAME)                  METHOD_PROTO(MOD_NAME, METHOD_UPDATE_M_NAME)
-//#define METHOD_UPDATE_IMPL(MOD_NAME, THIS)             METHOD_IMPL(MOD_NAME, THIS, METHOD_UPDATE_M_NAME)
+//#define METHOD_UPDATE_PROTO(MOD_NAME)                  METHOD_PROTO(MOD_NAME, METHOD_UPDATE_M_NAME, METHOD_UPDATE_RET)
+//#define METHOD_UPDATE_IMPL(MOD_NAME, THIS)             METHOD_IMPL(MOD_NAME, THIS, METHOD_UPDATE_M_NAME, METHOD_UPDATE_RET)
 //#define UPDATE(MOD)                                     CALL(MOD, METHOD_UPDATE_M_NAME)
 
 // Метод idle().
 // Низкоприоритетные работа, вычисления модуля.
 #define METHOD_IDLE_M_NAME idle
-#define METHOD_IDLE(MOD_TYPE)                        METHOD(MOD_TYPE, METHOD_IDLE_M_NAME)
+#define METHOD_IDLE_RET void
+#define METHOD_IDLE(MOD_TYPE)                        METHOD(MOD_TYPE, METHOD_IDLE_M_NAME, METHOD_IDLE_RET)
 #define METHOD_IDLE_PTR(MOD_NAME)                    METHOD_PTR(MOD_NAME, METHOD_IDLE_M_NAME)
-#define METHOD_IDLE_PROTO(MOD_NAME)                  METHOD_PROTO(MOD_NAME, METHOD_IDLE_M_NAME)
-#define METHOD_IDLE_IMPL(MOD_NAME, THIS)             METHOD_IMPL(MOD_NAME, THIS, METHOD_IDLE_M_NAME)
+#define METHOD_IDLE_PROTO(MOD_NAME)                  METHOD_PROTO(MOD_NAME, METHOD_IDLE_M_NAME, METHOD_IDLE_RET)
+#define METHOD_IDLE_IMPL(MOD_NAME, THIS)             METHOD_IMPL(MOD_NAME, THIS, METHOD_IDLE_M_NAME, METHOD_IDLE_RET)
 #define IDLE(MOD)                                     CALL(MOD, METHOD_IDLE_M_NAME)
 
 // Метод cntl().
 // Управление модулем беза расчёта.
 #define METHOD_CONTROL_M_NAME cntl
-#define METHOD_CONTROL(MOD_TYPE)                        METHOD(MOD_TYPE, METHOD_CONTROL_M_NAME)
+#define METHOD_CONTROL_RET void
+#define METHOD_CONTROL(MOD_TYPE)                        METHOD(MOD_TYPE, METHOD_CONTROL_M_NAME, METHOD_CONTROL_RET)
 #define METHOD_CONTROL_PTR(MOD_NAME)                    METHOD_PTR(MOD_NAME, METHOD_CONTROL_M_NAME)
-#define METHOD_CONTROL_PROTO(MOD_NAME)                  METHOD_PROTO(MOD_NAME, METHOD_CONTROL_M_NAME)
-#define METHOD_CONTROL_IMPL(MOD_NAME, THIS)             METHOD_IMPL(MOD_NAME, THIS, METHOD_CONTROL_M_NAME)
+#define METHOD_CONTROL_PROTO(MOD_NAME)                  METHOD_PROTO(MOD_NAME, METHOD_CONTROL_M_NAME, METHOD_CONTROL_RET)
+#define METHOD_CONTROL_IMPL(MOD_NAME, THIS)             METHOD_IMPL(MOD_NAME, THIS, METHOD_CONTROL_M_NAME, METHOD_CONTROL_RET)
 #define CONTROL(MOD)                                     CALL(MOD, METHOD_CONTROL_M_NAME)
 
 //// Метод start().
 //#define METHOD_START_M_NAME start
-//#define METHOD_START(MOD_TYPE)                        METHOD(MOD_TYPE, METHOD_START_M_NAME)
+//#define METHOD_START_RET void
+//#define METHOD_START(MOD_TYPE)                        METHOD(MOD_TYPE, METHOD_START_M_NAME, METHOD_START_RET)
 //#define METHOD_START_PTR(MOD_NAME)                    METHOD_PTR(MOD_NAME, METHOD_START_M_NAME)
-//#define METHOD_START_PROTO(MOD_NAME)                  METHOD_PROTO(MOD_NAME, METHOD_START_M_NAME)
-//#define METHOD_START_IMPL(MOD_NAME, THIS)             METHOD_IMPL(MOD_NAME, THIS, METHOD_START_M_NAME)
+//#define METHOD_START_PROTO(MOD_NAME)                  METHOD_PROTO(MOD_NAME, METHOD_START_M_NAME, METHOD_START_RET)
+//#define METHOD_START_IMPL(MOD_NAME, THIS)             METHOD_IMPL(MOD_NAME, THIS, METHOD_START_M_NAME, METHOD_START_RET)
 //#define START(MOD)                                    CALL(MOD, METHOD_START_M_NAME)
 
 //// Метод stop().
 //#define METHOD_STOP_M_NAME stop
-//#define METHOD_STOP(MOD_TYPE)                        METHOD(MOD_TYPE, METHOD_STOP_M_NAME)
+//#define METHOD_STOP_RET void
+//#define METHOD_STOP(MOD_TYPE)                        METHOD(MOD_TYPE, METHOD_STOP_M_NAME, METHOD_STOP_RET)
 //#define METHOD_STOP_PTR(MOD_NAME)                    METHOD_PTR(MOD_NAME, METHOD_STOP_M_NAME)
-//#define METHOD_STOP_PROTO(MOD_NAME)                  METHOD_PROTO(MOD_NAME, METHOD_STOP_M_NAME)
-//#define METHOD_STOP_IMPL(MOD_NAME, THIS)             METHOD_IMPL(MOD_NAME, THIS, METHOD_STOP_M_NAME)
+//#define METHOD_STOP_PROTO(MOD_NAME)                  METHOD_PROTO(MOD_NAME, METHOD_STOP_M_NAME, METHOD_STOP_RET)
+//#define METHOD_STOP_IMPL(MOD_NAME, THIS)             METHOD_IMPL(MOD_NAME, THIS, METHOD_STOP_M_NAME, METHOD_STOP_RET)
 //#define STOP(MOD)                                     CALL(MOD, METHOD_STOP_M_NAME)
 
 
