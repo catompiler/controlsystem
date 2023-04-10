63bc4595ff1f80e14b31f3b6f809d8e194be5efb
artem.lab <artem.lab@gmail.com>
2023-04-10 11:56:24 +0500

    iqmath iq iq_n_to_m macro

--- iqmath/iq.h
+++ iqmath/iq.h
@@ -6,5 +6,8 @@
 #include "iqn.h"
 #include "iq_intrinsics.h"
 
+//! Преобразовывает число с фиксированной запятой
+//! из формата Qn в формат Qm.
+#define IQ_N_TO_M(Q, N, M) IQN_N_TO_M(Q, N, M)
 
 #endif /* IQ_H */
