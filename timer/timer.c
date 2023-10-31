7f7bf213c4624975fecbe727cdab2bc0be85149e
artem.lab <artem.lab@gmail.com>
2023-10-31 10:19:53 +0500

    timer module

---
+++ timer/timer.c
@@ -0,0 +1,55 @@
+#include "timer.h"
+#include "modules/modules.h"
+
+
+METHOD_INIT_IMPL(M_timer, tim)
+{
+}
+
+METHOD_DEINIT_IMPL(M_timer, tim)
+{
+}
+
+static uint32_t timer_calc_diff(uint32_t ref, uint32_t cur)
+{
+    // Текущее значение больше опорного - счётчик не переполнился.
+    if(cur >= ref) return cur - ref;
+    // Иначе cur < ref - счётчик переполнился.
+    return UINT32_MAX - ref + cur + 1;
+}
+
+METHOD_CALC_IMPL(M_timer, tim)
+{
+    tim->out_timeout = STROBE_NONE;
+
+    if(tim->status & STATUS_RUN){
+        // Пройденное время с начала счёта.
+        uint32_t elapsed_time = timer_calc_diff(tim->m_ref_counter, sys_time.r_counter_ms);
+        // Если времени прошло больше чем интервал таймера.
+        if(elapsed_time >= tim->r_interval){
+            tim->status = STATUS_NONE;
+
+            tim->out_expired = FLAG_ACTIVE;
+            tim->out_timeout = STROBE_ACTIVE;
+        }
+    }
+}
+
+METHOD_CONTROL_IMPL(M_timer, tim)
+{
+//    if((tim->status & STATUS_RUN) && (tim->control & CONTROL_RESET)){
+//        tim->m_ref_counter = sys_time.r_counter_ms;
+//    }
+    if(tim->control & CONTROL_START){
+        tim->m_ref_counter = sys_time.r_counter_ms;
+        tim->out_expired = FLAG_NONE;
+        tim->out_timeout = STROBE_NONE;
+
+        tim->status = STATUS_RUN;
+    }
+    if(tim->control & CONTROL_STOP){
+        tim->status = STATUS_NONE;
+    }
+
+    tim->control = CONTROL_NONE;
+}
---
+++ timer/timer.h
@@ -0,0 +1,77 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "module/base.h"
+
+
+//! Интервал по-умолчанию.
+#define TIMER_INTERVAL_DEFAULT 0
+
+
+//! Перечисление возможных бит управления.
+enum _E_Timer_Control {
+    TIMER_CONTROL_NONE = CONTROL_NONE,
+//    TIMER_CONTROL_RESET = CONTROL_RESET,
+    TIMER_CONTROL_START = CONTROL_START,
+    TIMER_CONTROL_STOP = CONTROL_STOP,
+};
+
+//! Перечисление возможных бит статуса.
+enum _E_Timer_Status {
+    TIMER_STATUS_NONE = STATUS_NONE,
+};
+
+//! Предварительная декларация типа модуля.
+typedef struct _S_Timer M_timer;
+
+//! Структура модуля.
+struct _S_Timer {
+    // Базовые поля.
+    control_t control; //!< Слово управления.
+    status_t status; //!< Слово состояния.
+    // Входные данные.
+    //reg_u32_t in_counter; //!< Значение опорного счётчика.
+    // Выходные данные.
+    flag_t out_expired; //!< Флаг истечения времени.
+    strobe_t out_timeout; //!< Строб тайм-аута.
+    // Параметры.
+    // Регистры.
+    reg_u32_t r_interval; //!< Интервал таймера, мс.
+    // Методы.
+    METHOD_INIT(M_timer);
+    METHOD_DEINIT(M_timer);
+    METHOD_CALC(M_timer);
+    METHOD_CONTROL(M_timer);
+    // Коллбэки.
+    // Внутренние данные.
+    uint32_t m_ref_counter; //!< Опорное значение счётчика.
+};
+
+EXTERN METHOD_INIT_PROTO(M_timer);
+EXTERN METHOD_DEINIT_PROTO(M_timer);
+EXTERN METHOD_CALC_PROTO(M_timer);
+EXTERN METHOD_CONTROL_PROTO(M_timer);
+
+#define TIMER_DEFCFG(INTERVAL) {\
+        /* Базовые поля */\
+        0, 0, /* control, status */\
+        /* Входные данные */\
+        /*0,*/ /* in_counter */\
+        /* Выходные данные */\
+        0, /* out_expired */\
+        0, /* out_timeout */\
+        /* Параметры */\
+        /* Регистры */\
+        INTERVAL, /* r_interval */\
+        /* Методы */\
+        METHOD_INIT_PTR(M_timer), METHOD_DEINIT_PTR(M_timer),\
+        METHOD_CALC_PTR(M_timer),\
+        METHOD_CONTROL_PTR(M_timer),\
+        /* Коллбэки */\
+        /* Внутренние данные */\
+        0, /* m_ref_counter */\
+    }
+
+#define TIMER_DEFAULTS TIMER_DEFCFG(TIMER_INTERVAL_DEFAULT)
+
+#endif /* TIMER_H */
