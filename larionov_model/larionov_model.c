c04b8dc3f2b583afb818c25bdab8897372617444
artem.lab <artem.lab@gmail.com>
2023-10-31 10:19:02 +0500

    three-phase rectifier model with loads

---
+++ larionov_model/larionov_model.c
@@ -0,0 +1,650 @@
+#include "larionov_model.h"
+#include "modules/modules.h"
+#include "iqmath/iqmath.h"
+#include "conf/conf.h"
+
+//
+// Модели.
+//
+
+//
+// Модель активной нагрузки.
+//
+
+#define LARIONOV_MODEL_LOAD_R_R_DEFAULT IQ15(1)
+
+// Вычисление.
+static void larionov_model_load_R_calc(M_larionov_model *lrm)
+{
+    iq24_t U = lrm->m_U_rect;
+
+    iq24_t I = iq24_div(U, lrm->m_load_r_R);
+
+    if (I < lrm->m_I_hold) {
+        U = 0;
+        I = 0;
+    }
+
+    lrm->m_load_r_U = U;
+    lrm->m_load_r_I = I;
+}
+
+// Сброс.
+static void larionov_model_load_R_reset(M_larionov_model *lrm)
+{
+    lrm->m_load_r_U = 0;
+    lrm->m_load_r_I = 0;
+}
+
+// Обновление констант.
+static status_t larionov_model_load_R_update(M_larionov_model *lrm)
+{
+    status_t res_status = STATUS_READY;
+
+    iq15_t R = lrm->p_load_r_R;
+
+    if(R <= 0){
+        res_status = STATUS_ERROR;
+        R = LARIONOV_MODEL_LOAD_R_R_DEFAULT;
+    }
+
+    lrm->m_load_r_R = iq15_mul(R, conf.r_R_base_inv);
+
+    return res_status;
+}
+
+//
+// Модель активно-индуктивной нагрузки.
+//
+
+#define LARIONOV_MODEL_LOAD_LR_L_DEFAULT IQ15(0.001)
+
+// Вычисление.
+static void larionov_model_load_RL_calc(M_larionov_model *lrm)
+{
+    if(lrm->m_load_rl_L == 0){
+        return;
+    }
+
+    iq24_t tmp;
+
+    iq24_t U = lrm->m_U_rect;
+
+    // dI = (U - I * R) * dt / L.
+    iq24_t dI;
+    // I * R.
+    tmp = iq24_mul(lrm->m_load_rl_I, lrm->m_load_rl_R);
+    // U - (I * R).
+    tmp = iq24_sub(U, tmp);
+    // (U - (I * R) * dt.
+    tmp = iq24_mul(tmp, lrm->m_cur_dt);
+    // * t_base.
+    tmp = iq24_mul(tmp, conf.r_t_base);
+    // (U - (I * R) * dt / L.
+    dI = iq24_div(tmp, lrm->m_load_rl_L);
+
+
+    iq24_t I = lrm->m_load_rl_I + dI;
+
+    if (I < lrm->m_I_hold) {
+        U = 0;
+        I = 0;
+    }
+
+    lrm->m_load_rl_U = U;
+    lrm->m_load_rl_I = I;
+}
+
+// Сброс.
+static void larionov_model_load_RL_reset(M_larionov_model *lrm)
+{
+    lrm->m_load_rl_U = 0;
+    lrm->m_load_rl_I = 0;
+}
+
+// Обновление констант.
+static status_t larionov_model_load_RL_update(M_larionov_model *lrm)
+{
+    status_t res_status = STATUS_READY;
+
+    iq15_t R = lrm->p_load_rl_R;
+    iq15_t L = lrm->p_load_rl_L;
+
+    if(L == 0){
+        res_status = STATUS_ERROR;
+        L = LARIONOV_MODEL_LOAD_LR_L_DEFAULT;
+    }
+
+    lrm->m_load_rl_R = iq15_mul(R, conf.r_R_base_inv);
+    lrm->m_load_rl_L = iq15_mul(L, conf.r_L_base_inv);
+
+    return res_status;
+}
+
+//
+// Модель нагрузки ДПТ.
+//
+
+#define LARIONOV_MODEL_LOAD_DCM_U_NOM_DEFAULT IQ15(440)
+#define LARIONOV_MODEL_LOAD_DCM_I_NOM_DEFAULT IQ15(361)
+#define LARIONOV_MODEL_LOAD_DCM_N_NOM_DEFAULT IQ15(1475)
+#define LARIONOV_MODEL_LOAD_DCM_R_DEFAULT IQ15(0.1)
+#define LARIONOV_MODEL_LOAD_DCM_L_DEFAULT IQ15(0.01)
+#define LARIONOV_MODEL_LOAD_DCM_Mr_DEFAULT IQ15(1)
+#define LARIONOV_MODEL_LOAD_DCM_J_DEFAULT IQ15(1)
+#define LARIONOV_MODEL_LOAD_DCM_Tj_DEFAULT IQ24(1)
+#define LARIONOV_MODEL_LOAD_DCM_Wnom_pu_DEFAULT IQ24(1475.0/3000)
+
+// Вычисление.
+static void larionov_model_load_DCM_calc(M_larionov_model *lrm)
+{
+    if(lrm->m_load_dcm_L == 0 || lrm->m_load_dcm_Tj){
+        return;
+    }
+
+    iq24_t tmp;
+
+    iq24_t U = lrm->m_U_rect;
+
+    // E = kF * w.
+    iq24_t E = iq24_mul(lrm->m_load_dcm_kF, lrm->m_load_dcm_W);
+
+    // dI = (U - I * R) * dt / L.
+    iq24_t dI;
+    // I * R.
+    tmp = iq24_mul(lrm->m_load_dcm_I, lrm->m_load_dcm_R);
+    // U - E - (I * R).
+    // E + (I * R).
+    tmp = iq24_add(E, tmp);
+    // U - (E + I * R).
+    tmp = iq24_sub(U, tmp);
+    // (U - (I * R) * dt.
+    tmp = iq24_mul(tmp, lrm->m_cur_dt);
+    // * t_base.
+    tmp = iq24_mul(tmp, conf.r_t_base);
+    // (U - (I * R) * dt / L.
+    dI = iq24_div(tmp, lrm->m_load_dcm_L);
+
+    iq24_t I = lrm->m_load_dcm_I + dI;
+
+    if (I < lrm->m_I_hold) {
+        U = E;
+        I = 0;
+    }
+
+    iq24_t M = iq24_mul(lrm->m_load_dcm_kF, lrm->m_load_dcm_I);
+
+    // dW = (M - Mc) * dt / J.
+    iq24_t dW;
+    // M - Mc.
+    tmp = iq24_sub(M, lrm->m_load_dcm_Mr);
+    // (M - Mc) * dt.
+    tmp = iq24_mul(tmp, lrm->m_cur_dt);
+    // * t_base.
+    tmp = iq24_mul(tmp, conf.r_t_base);
+    // dW = (M - Mc) * dt / Tj.
+    dW = iq24_div(tmp, lrm->m_load_dcm_Tj);
+
+    iq24_t W = lrm->m_load_dcm_W + dW;
+    if (W < 0) {
+        W = 0;
+    }
+
+    lrm->m_load_dcm_U = U;
+    lrm->m_load_dcm_I = I;
+    lrm->m_load_dcm_W = W;
+}
+
+// Сброс.
+static void larionov_model_load_DCM_reset(M_larionov_model *lrm)
+{
+    lrm->m_load_dcm_U = 0;
+    lrm->m_load_dcm_I = 0;
+    lrm->m_load_dcm_W = 0;
+}
+
+// Обновление констант.
+static status_t larionov_model_load_DCM_update(M_larionov_model *lrm)
+{
+    status_t res_status = STATUS_READY;
+
+    iql_t tmp;
+
+    iq15_t Unom = lrm->p_load_dcm_Unom;
+    iq15_t Inom = lrm->p_load_dcm_Inom;
+    iq15_t Nnom = lrm->p_load_dcm_Nnom;
+    iq15_t R = lrm->p_load_dcm_R;
+    iq15_t L = lrm->p_load_dcm_L;
+    iq15_t Mr = lrm->p_load_dcm_Mr;
+    iq15_t J = lrm->p_load_dcm_J;
+
+    if(Unom <= 0){
+        res_status = STATUS_ERROR;
+        Unom = LARIONOV_MODEL_LOAD_DCM_U_NOM_DEFAULT;
+    }
+
+    if(Inom <= 0){
+        res_status = STATUS_ERROR;
+        Inom = LARIONOV_MODEL_LOAD_DCM_I_NOM_DEFAULT;
+    }
+
+    if(Nnom <= 0){
+        res_status = STATUS_ERROR;
+        Nnom = LARIONOV_MODEL_LOAD_DCM_N_NOM_DEFAULT;
+    }
+
+    if(R < 0){
+        res_status = STATUS_ERROR;
+        R = LARIONOV_MODEL_LOAD_DCM_R_DEFAULT;
+    }
+
+    if(L <= 0){
+        res_status = STATUS_ERROR;
+        L = LARIONOV_MODEL_LOAD_DCM_L_DEFAULT;
+    }
+
+    if(Mr < 0){
+        res_status = STATUS_ERROR;
+        Mr = LARIONOV_MODEL_LOAD_DCM_Mr_DEFAULT;
+    }
+
+    if(J <= 0){
+        res_status = STATUS_ERROR;
+        J = LARIONOV_MODEL_LOAD_DCM_J_DEFAULT;
+    }
+
+    lrm->m_load_dcm_Unom = iq15_mul(Unom, conf.r_U_base_inv); // mul(q15, q24) == q24.
+    lrm->m_load_dcm_Inom = iq15_mul(Inom, conf.r_I_base_inv); // mul(q15, q24) == q24.
+    lrm->m_load_dcm_R = iq15_mul(R, conf.r_R_base_inv); // mul(q15, q24) == q24.
+    lrm->m_load_dcm_L = iq15_mul24(L, conf.r_L_base_inv); // mul15_24(q15, q15) == q24.
+    lrm->m_load_dcm_Mr = iq15_mul(Mr, conf.r_M_base_inv); // mul(q15, q24) == q24.
+
+    // Wnom = PI * N / 30.
+    iq15_t Wnom;
+    // N / 30.
+    tmp = iq15_idiv(Nnom, 30);
+    // PI * (N / 30).
+    Wnom = iq15_mul(tmp, IQ15_PI);
+
+    // http://simenergy.ru/energy-system/primary-equipment/equation-rotor
+    // Tj = J * w^2 / S.
+    // S = Unom * Inom.
+    // J * w.
+    tmp = iq15_mul(J, Wnom);
+    // (J * w) / Unom.
+    tmp = iq15_div(tmp, Unom);
+    // (J * w / Unom) * w.
+    tmp = iq15_mul(tmp, Wnom);
+    // (J * w^2 / Unom) / Inom.
+    // IQ(24) = IQ(15 + 24 - 15).
+    lrm->m_load_dcm_Tj = iq24_div(tmp, Inom);
+
+    if(lrm->m_load_dcm_Tj <= 0){
+        res_status = STATUS_ERROR;
+        lrm->m_load_dcm_Tj = LARIONOV_MODEL_LOAD_DCM_Tj_DEFAULT;
+    }
+
+    // Wnom = PI * N / 30.
+    iq24_t Wnom_pu;
+    // N / 30.
+    //tmp = iq15_idiv(Nnom, 30);
+    // PI * (N / 30).
+    //tmp = iq15_mul(tmp, IQ15_PI);
+    // /w_base.
+    Wnom_pu = iq15_mul(Wnom, conf.r_w_base_inv);
+    if(Wnom_pu == 0){
+        res_status = STATUS_ERROR;
+        Wnom_pu = LARIONOV_MODEL_LOAD_DCM_Wnom_pu_DEFAULT;
+    }
+
+    // kF = (U - I * R) / Wnom.
+    iq24_t kF;
+    // I * R.
+    tmp = iq24_mul(lrm->m_load_dcm_Inom, lrm->m_load_dcm_R);
+    // U - (I * R).
+    tmp = iq24_sub(lrm->m_load_dcm_Unom, tmp);
+    // (U - I * R) / Wnom.
+    kF = iq24_div(tmp, Wnom_pu);
+
+    lrm->m_load_dcm_kF = kF;
+
+    return res_status;
+}
+
+// Проверяет наличие хотя бы одного флага в массиве.
+static flag_t larionov_model_has_active_state(flag_t *f)
+{
+    int i;
+    for (i = 0; i < LARIONOV_MODEL_KEYS_COUNT; i++) {
+        if (f[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// Проверяет наличие хотя бы одного флага в массиве.
+static void larionov_model_reset_state(flag_t *f)
+{
+    int i;
+    for (i = 0; i < LARIONOV_MODEL_KEYS_COUNT; i++) {
+        f[i] = 0;
+    }
+}
+
+static int larionov_model_next_scr_index(int index)
+{
+    int next_index = index - 1;
+
+    if (next_index < 0) {
+        next_index += LARIONOV_MODEL_KEYS_COUNT;
+    }
+
+    return next_index;
+}
+
+static void larionov_model_set_states(flag_t* out_states, flag_t* in_states)
+{
+    int i;
+    for (i = 0; i < LARIONOV_MODEL_KEYS_COUNT; i++) {
+        out_states[i] = in_states[i];
+    }
+}
+
+// Обновляет управляющие.
+static void larionov_model_update_gates(M_larionov_model *lrm)
+{
+    // Флаг наличия управляющих.
+    flag_t has_gates = larionov_model_has_active_state(lrm->m_gates_state);
+    // Если есть.
+    if (has_gates) {
+        // Угол с начала подачи управления.
+        iq24_t gates_da = iq24_angle_norm_pu(
+                lrm->m_cur_Uref_angle - lrm->m_gates_ref_angle);
+        // Если превышает заданную продолжительность.
+        if (gates_da > lrm->in_control_duration_angle_pu) {
+            // Сбросим управляющие.
+            larionov_model_reset_state(lrm->m_gates_state);
+        }
+    }
+
+}
+
+// Обновляет выходное напряжение.
+static void larionov_model_update_voltage_and_state(M_larionov_model *lrm)
+{
+    // [ AB, AC, BC, BA, CA, CB ]
+    iq24_t voltages[LARIONOV_MODEL_KEYS_COUNT] = { lrm->m_cur_Uab,
+            -lrm->m_cur_Uca, lrm->m_cur_Ubc, -lrm->m_cur_Uab, lrm->m_cur_Uca,
+            -lrm->m_cur_Ubc };
+
+    int i;
+
+    int pair_first = -1;
+    int pair_second = -1;
+    iq24_t U = 0;
+
+    // Выбор максимального напряжения открытых пар тиристоров и соответствующей пары.
+    for (i = 0; i < LARIONOV_MODEL_KEYS_COUNT; i++) {
+        int self_index = i;
+        int pair_index = larionov_model_next_scr_index(i);
+
+        flag_t self_gated = lrm->m_gates_state[self_index];
+        flag_t pair_gated = lrm->m_gates_state[pair_index];
+
+        flag_t self_opened = lrm->m_scrs_state[self_index];
+        flag_t pair_opened = lrm->m_scrs_state[pair_index];
+
+        iq24_t voltage = voltages[i];
+
+        if ((self_gated != 0 && pair_gated != 0 && voltage > 0)
+                || (self_opened != 0 && pair_opened != 0)) {
+            if (voltage > U || pair_first == -1) {
+                pair_first = self_index;
+                pair_second = pair_index;
+                U = voltage;
+            }
+        }
+    }
+
+    lrm->m_U_rect = U;
+
+    // Установка состояния тиристоров.
+    for (i = 0; i < LARIONOV_MODEL_KEYS_COUNT; i++) {
+        if (i == pair_first || i == pair_second) {
+            lrm->m_scrs_state[i] = 1;
+        } else {
+            lrm->m_scrs_state[i] = 0;
+        }
+    }
+
+}
+
+// Вычисляет нагрузку, в зависимости от типа.
+static void larionov_model_calc_load(M_larionov_model *lrm)
+{
+    if (lrm->p_load_type == 0) {
+        larionov_model_load_R_calc(lrm);
+
+        lrm->m_U_load = lrm->m_load_r_U;
+        lrm->m_I_load = lrm->m_load_r_I;
+    } else if (lrm->p_load_type == 1) {
+        larionov_model_load_RL_calc(lrm);
+
+        lrm->m_U_load = lrm->m_load_rl_U;
+        lrm->m_I_load = lrm->m_load_rl_I;
+    } else if (lrm->p_load_type == 2) {
+        larionov_model_load_DCM_calc(lrm);
+
+        lrm->m_U_load = lrm->m_load_dcm_U;
+        lrm->m_I_load = lrm->m_load_dcm_I;
+    }
+}
+
+// Обновляет токи и состояния тиристоров.
+static void larionov_model_update_currents_and_state(M_larionov_model *lrm)
+{
+    // Модель всегда "отпирает" только два тиристора,
+    // Поэтому считать число открытых тиристоров не нужно,
+    // Ток через каждый открытый тиристор
+    // будет всегда равен току нагрузки.
+
+    int i;
+
+    // Обновление тока тиристоров.
+    for (i = 0; i < LARIONOV_MODEL_KEYS_COUNT; i++) {
+        if (lrm->m_scrs_state[i] != 0) {
+            lrm->m_scrs_I[i] = lrm->m_I_load;
+        }
+    }
+
+    // Обновление состояния тиристоров.
+    for (i = 0; i < LARIONOV_MODEL_KEYS_COUNT; i++) {
+        if (lrm->m_scrs_I[i] < lrm->p_I_hold) {
+            lrm->m_scrs_state[i] = 0;
+        }
+    }
+}
+
+// Обновляет выходные переменные.
+static void larionov_model_update_outputs(M_larionov_model *lrm)
+{
+    lrm->out_Iab = lrm->m_scrs_I[1] - lrm->m_scrs_I[4];
+    lrm->out_Ibc = lrm->m_scrs_I[3] - lrm->m_scrs_I[6];
+    lrm->out_Ica = lrm->m_scrs_I[5] - lrm->m_scrs_I[2];
+
+    lrm->out_U = lrm->m_U_load;
+    lrm->out_I = lrm->m_I_load;
+}
+
+// Выполняет расчёт модели.
+static void larionov_model_sim(M_larionov_model *lrm)
+{
+    // Обновим состояния управляющих.
+    larionov_model_update_gates(lrm);
+
+    // Обновим напряжения и состояния.
+    larionov_model_update_voltage_and_state(lrm);
+
+    // Вычисление модели.
+    larionov_model_calc_load(lrm);
+
+    // Обработаем ток модели.
+    larionov_model_update_currents_and_state(lrm);
+}
+
+static status_t recalc_values(M_larionov_model* lrm)
+{
+    status_t res_status = STATUS_READY;
+    status_t status = STATUS_NONE;
+
+    lrm->m_I_hold = iq15_mul(lrm->p_I_hold, conf.r_I_base_inv);
+
+    // Обновим рассчитываемые константы.
+    status = larionov_model_load_R_update(lrm);
+    if(status != STATUS_READY) { res_status = status; }
+
+    status = larionov_model_load_RL_update(lrm);
+    if(status != STATUS_READY) { res_status = status; }
+
+    status = larionov_model_load_DCM_update(lrm);
+    if(status != STATUS_READY) { res_status = status; }
+
+    return res_status;
+}
+
+METHOD_INIT_IMPL(M_larionov_model, lrm)
+{
+    IDLE((*lrm));
+}
+
+METHOD_DEINIT_IMPL(M_larionov_model, lrm)
+{
+}
+
+METHOD_CALC_IMPL(M_larionov_model, lrm)
+{
+    // Обработка управления.
+
+    // Если разрешён расчёт.
+    if (lrm->control & CONTROL_ENABLE) {
+        // Флаг начала подачи управления.
+        flag_t control_occurred = 0;
+        // Доля времени до подачи управления.
+        iq24_t dt_ratio = 0.0;
+
+        // Есть ли управление?
+        flag_t has_control = larionov_model_has_active_state(lrm->in_control);
+        // Если есть новое управление -
+        // Сохраним для обработки.
+        if (has_control) {
+            lrm->m_need_control = 1;
+            larionov_model_set_states(lrm->m_control, lrm->in_control);
+
+            lrm->m_control_delay_angle = lrm->in_control_delay_angle_pu;
+            lrm->m_control_ref_Uab = lrm->in_Uab;
+            lrm->m_control_ref_Ubc = lrm->in_Ubc;
+            lrm->m_control_ref_Uca = lrm->in_Uca;
+            lrm->m_control_ref_angle = lrm->in_Uref_angle_pu;
+        }
+
+        // Если есть текущее управление.
+        if (lrm->m_need_control == 1) {
+            // Прошедший угол с момента получения управления.
+            iq24_t control_da = iq24_angle_norm_pu(
+                    lrm->in_Uref_angle_pu - lrm->m_control_ref_angle);
+
+            // Если с момента получения произошло управление.
+            if (control_da > lrm->m_control_delay_angle) {
+                // Сбросим флаг необходимости управления.
+                lrm->m_need_control = 0;
+
+                // Доля времени до подачи упраления.
+                dt_ratio = lrm->m_control_delay_angle / control_da;
+
+                // Установим флаг того что управление произошло.
+                control_occurred = 1;
+
+            } else {
+                // Иначе сохраним текущий момент как опорный.
+                lrm->m_control_delay_angle = lrm->m_control_delay_angle
+                        - control_da;
+                lrm->m_control_ref_Uab = lrm->in_Uab;
+                lrm->m_control_ref_Ubc = lrm->in_Ubc;
+                lrm->m_control_ref_Uca = lrm->in_Uca;
+                lrm->m_control_ref_angle = lrm->in_Uref_angle_pu;
+            }
+
+        }
+
+        // Расчёт.
+        // Установим текущий интервал времени.
+        lrm->m_cur_dt = lrm->in_dt;
+
+        // Если произошло управление.
+        if (control_occurred) {
+            // Установим текущий угол.
+            lrm->m_cur_Uref_angle = lrm->m_control_ref_angle
+                    + lrm->m_control_delay_angle;
+            // Установим интервал времени до момент управления.
+            lrm->m_cur_dt = lrm->in_dt * dt_ratio;
+            // Проинтерполируем напряжения к моменту подачи управления.
+            lrm->m_cur_Uab = iq24_lerp(lrm->m_control_ref_Uab,
+                    lrm->in_Uab, dt_ratio);
+            lrm->m_cur_Ubc = iq24_lerp(lrm->m_control_ref_Ubc,
+                    lrm->in_Ubc, dt_ratio);
+            lrm->m_cur_Uca = iq24_lerp(lrm->m_control_ref_Uca,
+                    lrm->in_Uca, dt_ratio);
+
+            larionov_model_sim(lrm);
+
+            // Установим начало подачи управления.
+            larionov_model_set_states(lrm->m_gates_state, lrm->m_control);
+            lrm->m_gates_ref_angle = lrm->m_cur_Uref_angle;
+
+            // Установим интервал времени после момента управления.
+            lrm->m_cur_dt = lrm->in_dt - lrm->m_cur_dt;
+        }
+
+        // Установим текущий угол.
+        lrm->m_cur_Uref_angle = lrm->in_Uref_angle_pu;
+        // Устновим текущие значения напряжений.
+        lrm->m_cur_Uab = lrm->in_Uab;
+        lrm->m_cur_Ubc = lrm->in_Ubc;
+        lrm->m_cur_Uca = lrm->in_Uca;
+
+        // Симуляция модели.
+        larionov_model_sim(lrm);
+
+        // Обновление выходов.
+        larionov_model_update_outputs(lrm);
+
+    }
+}
+
+METHOD_IDLE_IMPL(M_larionov_model, lrm)
+{
+    // Сбросим неиспользуемые модели.
+    if (lrm->p_load_type != 0) {
+        larionov_model_load_R_reset(lrm);
+    }
+
+    if (lrm->p_load_type != 1) {
+        larionov_model_load_RL_reset(lrm);
+    }
+
+    if (lrm->p_load_type != 2) {
+        larionov_model_load_DCM_reset(lrm);
+    }
+
+    status_t status = recalc_values(lrm);
+
+    if(status == STATUS_READY){
+        lrm->warnings = LARIONOV_MODEL_WARNING_NONE;
+        lrm->status = STATUS_NONE;
+    }else{
+        lrm->warnings = LARIONOV_MODEL_WARNING_INVALID_PARAMS;
+        lrm->status = STATUS_WARNING;
+    }
+}
+
---
+++ larionov_model/larionov_model.h
@@ -0,0 +1,256 @@
+#ifndef LARIONOV_MODEL_H
+#define LARIONOV_MODEL_H
+
+#include "module/base.h"
+#include "iqmath/iqmath.h"
+#include "conf/conf.h"
+#include <stdint.h>
+
+
+
+//! Число фаз для управления.
+#define LARIONOV_MODEL_PHASES_COUNT 3
+
+//! Число ключей для управления.
+#define LARIONOV_MODEL_KEYS_COUNT (LARIONOV_MODEL_PHASES_COUNT * 2)
+
+//! Индексы управлений и ключей.
+//! Управления.
+//! Управление AB.
+#define LARIONOV_MODEL_AB 0
+//! Управление BC.
+#define LARIONOV_MODEL_BC 2
+//! Управление CA.
+#define LARIONOV_MODEL_CA 4
+//! Управление BA.
+#define LARIONOV_MODEL_BA 3
+//! Управление CB.
+#define LARIONOV_MODEL_CB 5
+//! Управление AC.
+#define LARIONOV_MODEL_AC 1
+//! Ключи.
+//! Ключ верхний фазы A.
+#define LARIONOV_MODEL_A_HI 0
+//! Ключ верхний фазы B.
+#define LARIONOV_MODEL_B_HI 2
+//! Ключ верхний фазы C.
+#define LARIONOV_MODEL_C_HI 4
+//! Ключ нижний фазы A.
+#define LARIONOV_MODEL_A_LO 3
+//! Ключ нижний фазы B.
+#define LARIONOV_MODEL_B_LO 5
+//! Ключ нижний фазы C.
+#define LARIONOV_MODEL_C_LO 1
+
+
+//! Продолжительность угла управления по-умолчанию.
+#define LARIONOV_MODEL_CONTROL_DURATION_ANGLE_DEFAULT (IQ24_PI_PU / 18)
+
+//! Интервал времени для вычисления модели по-умолчанию.
+#define LARIONOV_MODEL_DT_DEFAULT (IQ24(0.02 / CONF_PERIOD_SAMPLES))
+
+
+//! Перечисление возможных бит управления.
+enum _E_Larionov_Model_Control {
+    LARIONOV_MODEL_CONTROL_NONE = CONTROL_NONE,
+    LARIONOV_MODEL_CONTROL_ENABLE = CONTROL_ENABLE,
+};
+
+//! Перечисление возможных бит статуса.
+enum _E_Larionov_Model_Status {
+    LARIONOV_MODEL_STATUS_NONE = STATUS_NONE,
+    LARIONOV_MODEL_STATUS_WARNING = STATUS_WARNING,
+};
+
+//! Перечисление возможных бит предупреждений.
+enum _E_Larionov_Model_Warnings {
+    LARIONOV_MODEL_WARNING_NONE = WARNING_NONE,
+    // TODO: Larionov model warning handle.
+    LARIONOV_MODEL_WARNING_INVALID_PARAMS = (WARNING_USER << 0),
+};
+
+//! Предварительная декларация типа модуля.
+typedef struct _S_Larionov_Model M_larionov_model;
+
+//! Структура модуля.
+struct _S_Larionov_Model {
+    // Базовые поля.
+    control_t control; //!< Слово управления.
+    status_t status; //!< Слово состояния.
+    warning_t warnings; //!< Слово предупреждений.
+    // Входные данные.
+    reg_iq24_t in_Uab; //!< Напряжение фазы AB.
+    reg_iq24_t in_Ubc; //!< Напряжение фазы BC.
+    reg_iq24_t in_Uca; //!< Напряжение фазы CA.
+    reg_iq24_t in_Uref_angle_pu; //!< Угол опорной фазы, в периодических единицах, [0; 2*pi).
+    strobe_t in_control[LARIONOV_MODEL_KEYS_COUNT]; //!< Управление.
+    reg_iq24_t in_control_delay_angle_pu; //!< Угол до импульса управления, в периодических единицах.
+    reg_iq24_t in_control_duration_angle_pu; //!< Продолжительность (угол) управления, в периодических единицах.
+    reg_iq24_t in_dt; //!< Интервал времени между вычислениями для вычисления модели, с.
+    // Выходные данные.
+    reg_iq24_t out_Iab; //!< Ток фазы AB.
+    reg_iq24_t out_Ibc; //!< Ток фазы BC.
+    reg_iq24_t out_Ica; //!< Ток фазы CA.
+    reg_iq24_t out_U; //!< Выходное напряжение.
+    reg_iq24_t out_I; //!< Выходной ток.
+    // Параметры.
+    // Общие.
+    reg_iq15_t p_I_hold; //!< Ток удержания, А.
+    // Нагрузка.
+    reg_u32_t p_load_type; //!< Тип нагрузки.
+    // Сопротивление [0].
+    reg_iq15_t p_load_r_R; //!< Сопротивление, Ом.
+    // RL цепь [1].
+    reg_iq15_t p_load_rl_R; //!< Сопротивление, Ом.
+    reg_iq15_t p_load_rl_L; //!< Индуктивность, Гн.
+    // ДПТ [2].
+    reg_iq15_t p_load_dcm_Unom; //!< Номинальное напряжение, В.
+    reg_iq15_t p_load_dcm_Inom; //!< Номинальный ток, А.
+    reg_iq15_t p_load_dcm_Nnom; //!< Номинальные обороты, об/мин.
+    reg_iq15_t p_load_dcm_J; //!< Момент инерции, кг * м2.
+    reg_iq15_t p_load_dcm_Mr; //!< Момент сопротивления на валу, Н * м.
+    reg_iq15_t p_load_dcm_R; //!< Сопротивление, Ом.
+    reg_iq15_t p_load_dcm_L; //!< Индуктивность, Гн.
+    // Регистры.
+    // Методы.
+    METHOD_INIT(M_larionov_model);
+    METHOD_DEINIT(M_larionov_model);
+    METHOD_CALC(M_larionov_model);
+    METHOD_IDLE(M_larionov_model);
+    // Коллбэки.
+    // Внутренние данные.
+    flag_t m_need_control; //!< Флаг необходимости управления.
+    flag_t m_control[LARIONOV_MODEL_KEYS_COUNT]; //!< Сохранённое управление.
+    iq24_t m_control_ref_angle; //!< Угол, с которого отсчитывается управление.
+    iq24_t m_control_delay_angle; //!< Угол до начала управления.
+    iq24_t m_control_ref_Uab; //!< Напряжение фазы AB до управления.
+    iq24_t m_control_ref_Ubc; //!< Напряжение фазы BC до управления.
+    iq24_t m_control_ref_Uca; //!< Напряжение фазы CA до управления.
+    iq24_t m_cur_Uref_angle; //!< Используемый опорный угол.
+    iq24_t m_cur_dt; //!< Используемый интервал времени.
+    iq24_t m_cur_Uab; //!< Используемое напряжение фазы AB.
+    iq24_t m_cur_Ubc; //!< Используемое напряжение фазы BC.
+    iq24_t m_cur_Uca; //!< Используемое напряжение фазы CA.
+    iq24_t m_gates_ref_angle; //!< Угол начала управления.
+    flag_t m_gates_state[LARIONOV_MODEL_KEYS_COUNT]; //!< Состояния управляющих.
+    flag_t m_scrs_state[LARIONOV_MODEL_KEYS_COUNT]; //!< Состояния тиристоров.
+    iq24_t m_scrs_I[LARIONOV_MODEL_KEYS_COUNT]; //!< Токи тиристоров.
+    iq24_t m_U_rect; //!< Напряжение на выходе выпрямителя.
+    // Данные нагрузки.
+    // Общие.
+    iq24_t m_U_load; //!< Напряжение нагрузки.
+    iq24_t m_I_load; //!< Ток нагрузки.
+    iq24_t m_I_hold; //!< Ток удержания.
+    // R цепь.
+    iq24_t m_load_r_U; //!< Напряжение нагрузки.
+    iq24_t m_load_r_I; //!< Ток нагрузки.
+    iq24_t m_load_r_R; //!< Сопротивление.
+    // RL цепь.
+    iq24_t m_load_rl_U; //!< Напряжение нагрузки.
+    iq24_t m_load_rl_I; //!< Ток нагрузки.
+    iq24_t m_load_rl_R; //!< Сопротивление.
+    iq24_t m_load_rl_L; //!< Сопротивление.
+    // ДПТ.
+    iq24_t m_load_dcm_U; //!< Напряжение нагрузки.
+    iq24_t m_load_dcm_I; //!< Ток нагрузки.
+    iq24_t m_load_dcm_Unom; //!< Номинальное напряжение.
+    iq24_t m_load_dcm_Inom; //!< Номинальный ток.
+    iq24_t m_load_dcm_Tj; //!< Механическая инерционная постоянная.
+    iq24_t m_load_dcm_Mr; //!< Момент сопротивления на валу.
+    iq24_t m_load_dcm_R; //!< Сопротивление.
+    iq24_t m_load_dcm_L; //!< Индуктивность.
+    iq24_t m_load_dcm_W; //!< Угловая частота двигателя.
+    iq24_t m_load_dcm_kF; //!< Коэффициент связи двигателя.
+};
+
+EXTERN METHOD_INIT_PROTO(M_larionov_model);
+EXTERN METHOD_DEINIT_PROTO(M_larionov_model);
+EXTERN METHOD_CALC_PROTO(M_larionov_model);
+EXTERN METHOD_IDLE_PROTO(M_larionov_model);
+
+#define LARIONOV_MODEL_DEFAULTS {\
+        /* Базовые поля */\
+        0, 0, /* control, status */\
+        0, /* warnings*/\
+        /* Входные данные */\
+        0, /* in_Uab */\
+        0, /* in_Ubc */\
+        0, /* in_Uca */\
+        0, /* in_Uref_angle_pu */\
+        {0}, /* in_control */\
+        0, /* in_control_delay_angle_pu */\
+        0, /* in_control_duration_angle_pu */\
+        LARIONOV_MODEL_DT_DEFAULT, /* in_dt */\
+        /* Выходные данные */\
+        0, /* out_Iab */\
+        0, /* out_Ibc */\
+        0, /* out_Ica */\
+        0, /* out_U */\
+        0, /* out_I */\
+        /* Параметры */\
+        IQ15(0.05), /* p_I_hold */\
+        0, /* p_load_type */\
+        /* Сопротивление [0] */\
+        IQ15(10.0), /* p_load_r_R */\
+        /* RL цепь [1] */\
+        IQ15(0.2), /* p_load_rl_R */\
+        IQ15(0.01), /* p_load_rl_L */\
+        /* ДПТ [2] */\
+        IQ15(440), /* p_load_dcm_Unom */\
+        IQ15(361), /* p_load_dcm_Inom */\
+        IQ15(1475), /* p_load_dcm_Nnom */\
+        IQ15(4.5), /* p_load_dcm_J */\
+        IQ15(50), /* p_load_dcm_Mr */\
+        IQ15(0.2), /* p_load_dcm_R */\
+        IQ15(0.01), /* p_load_dcm_L */\
+        /* Регистры */\
+        /* Методы */\
+        METHOD_INIT_PTR(M_larionov_model), METHOD_DEINIT_PTR(M_larionov_model),\
+        METHOD_CALC_PTR(M_larionov_model), METHOD_IDLE_PTR(M_larionov_model),\
+        /* Коллбэки */\
+        /* Внутренние данные */\
+        0, /* m_need_control */\
+        {0}, /* m_control */\
+        0, /* m_control_ref_angle */\
+        0, /* m_control_delay_angle */\
+        0, /* m_control_ref_Uab */\
+        0, /* m_control_ref_Ubc */\
+        0, /* m_control_ref_Uca */\
+        0, /* m_cur_Uref_angle */\
+        0, /* m_cur_dt */\
+        0, /* m_cur_Uab */\
+        0, /* m_cur_Ubc */\
+        0, /* m_cur_Uca */\
+        0, /* m_gates_ref_angle */\
+        {0}, /* m_gates_state */\
+        {0}, /* m_scrs_state */\
+        {0}, /* m_scrs_I */\
+        0, /* m_U_rect */\
+        /* Данные нагрузки */\
+        /* Общие */\
+        0, /* m_U_load */\
+        0, /* m_I_load */\
+        0, /* m_I_hold */\
+        /* R цепь */\
+        0, /* m_load_r_U */\
+        0, /* m_load_r_I */\
+        0, /* m_load_r_R */\
+        /* RL цепь */\
+        0, /* m_load_rl_U */\
+        0, /* m_load_rl_I */\
+        0, /* m_load_rl_R */\
+        0, /* m_load_rl_L */\
+        /* ДПТ */\
+        0, /* m_load_dcm_U */\
+        0, /* m_load_dcm_I */\
+        0, /* m_load_dcm_Unom */\
+        0, /* m_load_dcm_Inom */\
+        0, /* m_load_dcm_Tj */\
+        0, /* m_load_dcm_Mr */\
+        0, /* m_load_dcm_R */\
+        0, /* m_load_dcm_L */\
+        0, /* m_load_dcm_W */\
+        0, /* m_load_dcm_kF */\
+    }
+
+#endif /* LARIONOV_MODEL_H */
