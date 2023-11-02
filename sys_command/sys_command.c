ec682b9c726cb86ec43f2873475785a3af7ad6d2
artem.lab <artem.lab@gmail.com>
2023-11-02 16:27:29 +0500

    sys command: sys input interface

---
+++ sys_command/sys_command.c
@@ -0,0 +1,14 @@
+#include "sys_command.h"
+
+
+METHOD_INIT_IMPL(M_sys_command, sys_cmd)
+{
+}
+
+METHOD_DEINIT_IMPL(M_sys_command, sys_cmd)
+{
+}
+
+METHOD_CALC_IMPL(M_sys_command, sys_cmd)
+{
+}
---
+++ sys_command/sys_command.h
@@ -0,0 +1,64 @@
+#ifndef SYS_COMMAND_H
+#define SYS_COMMAND_H
+
+#include "module/base.h"
+
+//! Перечисление возможных бит управления.
+enum _E_Sys_Command_Control {
+    SYS_COMMAND_CONTROL_NONE = CONTROL_NONE,
+};
+
+//! Перечисление возможных бит статуса.
+enum _E_Sys_Command_Status {
+    SYS_COMMAND_STATUS_NONE = STATUS_NONE,
+};
+
+//! Командное слово 0.
+enum _E_Sys_Command_Command0 {
+    SYS_COMMAND_COMMAND_NONE    = 0x00,
+    SYS_COMMAND_COMMAND_ON      = 0x01,
+    SYS_COMMAND_COMMAND_CONT_ON = 0x02,
+    SYS_COMMAND_COMMAND_RUN     = 0x04,
+};
+
+//! Предварительная декларация типа модуля.
+typedef struct _S_Sys_Command M_sys_command;
+
+//! Структура модуля.
+struct _S_Sys_Command {
+    // Базовые поля.
+    control_t control; //!< Слово управления.
+    status_t status; //!< Слово состояния.
+    // Входные данные.
+    // Выходные данные.
+    reg_u32_t out_command; //!< Выходное командное слово.
+    // Параметры.
+    // Регистры.
+    // Методы.
+    METHOD_INIT(M_sys_command);
+    METHOD_DEINIT(M_sys_command);
+    METHOD_CALC(M_sys_command);
+    // Коллбэки.
+    // Внутренние данные.
+};
+
+EXTERN METHOD_INIT_PROTO(M_sys_command);
+EXTERN METHOD_DEINIT_PROTO(M_sys_command);
+EXTERN METHOD_CALC_PROTO(M_sys_command);
+
+#define SYS_COMMAND_DEFAULTS {\
+        /* Базовые поля */\
+        0, 0, /* control, status */\
+        /* Входные данные */\
+        /* Выходные данные */\
+        0, /* out_control0 */\
+        /* Параметры */\
+        /* Регистры */\
+        /* Методы */\
+        METHOD_INIT_PTR(M_sys_command), METHOD_DEINIT_PTR(M_sys_command),\
+        METHOD_CALC_PTR(M_sys_command),\
+        /* Коллбэки */\
+        /* Внутренние данные */\
+    }
+
+#endif /* SYS_COMMAND_H */
