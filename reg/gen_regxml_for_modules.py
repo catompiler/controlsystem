92fa0aaf6e4078d9032887d7f770ea30aae279df
catompiler <artem.lab@gmail.com>
2025-12-11 16:31:20 +0500

    doxygen comments reglist generation attributes

--- reg/gen_regxml_for_modules.py
+++ reg/gen_regxml_for_modules.py
@@ -1,5 +1,6 @@
 #import cxxheaderparser as cxxhp
-from re import RegexFlag
+import re
+import json
 
 from cxxheaderparser.simple import parse_file, ParsedData
 from cxxheaderparser.options import ParserOptions
@@ -95,6 +96,10 @@ class Var:
     name: str
     count: int  # == 0 - var, > 0 - array.
     comment: str
+    id: int or None
+    rpdo: bool
+    tpdo: bool
+    base_name: str or None
 
     def __init__(self, vType: str, vName: str, vCount: int = 0):
         self.typename = vType
@@ -103,6 +108,10 @@ class Var:
         self.name = vName
         self.count = vCount
         self.comment = ""
+        self.id = None
+        self.rpdo = False
+        self.tpdo = False
+        self.base_name = None
 
     def __repr__(self):
         if self.count == 0:
@@ -154,33 +163,60 @@ def getComment(comment: str) -> str:
                  .replace("*/", "").strip()
     return com
 
+def getJson(doxygen_comment: str) -> tuple[str or None, map or None]:
+    json_data = None
+    if doxygen_comment is not None:
+        if not hasattr(getJson, "prog") or getJson.prog is None:
+            getJson.prog = re.compile(r"@(\{.*\})")
+        json_match = getJson.prog.search(doxygen_comment)
+        if json_match is not None:
+            # remove json from comment
+            doxygen_comment = doxygen_comment[0:json_match.span()[0]] + doxygen_comment[json_match.span()[1]:]
+            # parse json insertion
+            json_group = json_match.group(1)
+            try:
+                json_data = json.loads(json_group)
+            except json.JSONDecodeError as e:
+                print("Error decode json ", json_group, ": ", e, sep='')
+    return doxygen_comment, json_data
+
 
 def getVars(parse_data: ParsedData) -> list[Var]:
+    # regexp for json insertion
+    # @{"param0": value0, "param1": value1}
+    prog = re.compile(r"@(\{.*\})")
     variables: list[Var] = []
     for var in parse_data.namespace.variables:
         #print(var)
+        var_id = None
+        doxygen_comment, json_data = getJson(var.doxygen)
+        #process json insertion
+        if json_data is not None:
+            var_id = json_data.get("id", None)
         if isinstance(var.type, Type):  # or not isinstance(fld.type.typename.segments[0], NameSpecifier):
             var_name = var.name.segments[0].name
             var_type = var.type.typename.segments[0].name
-            var_comment = getComment(var.doxygen)
+            var_comment = getComment(doxygen_comment)
             v = Var(var_type, var_name)
             if var_name.startswith(PARAM_PREFIX):
                 v.is_param = True
             v.is_const = var.type.const
             v.comment = var_comment
+            v.id = var_id
             variables.append(v)
         elif isinstance(var.type, Array):
             #print(fld)
             arr_size = int(var.type.size.tokens[0].value)
             arr_type = var.type.array_of.typename.segments[0].name
             arr_name = var.name.segments[0].name
-            arr_comment = getComment(var.doxygen)
+            arr_comment = getComment(doxygen_comment)
             #print(arr_name)
-            a = Var(arr_type, arr_name, arr_size)
+            v = Var(arr_type, arr_name, arr_size)
             if arr_name.startswith(PARAM_PREFIX):
-                a.is_param = True
-            a.comment = arr_comment
-            variables.append(a)
+                v.is_param = True
+            v.comment = arr_comment
+            v.id = var_id
+            variables.append(v)
 
     return variables
 
@@ -230,17 +266,32 @@ def collectData(header: str) -> list[Struct]:
                     break
             if excludeByPrefix:
                 continue
+            fld_id = None
+            fld_rpdo = False
+            fld_tpdo = False
+            fld_base_name = None
+            doxygen_comment, json_data = getJson(fld.doxygen)
+            #process json insertion
+            if json_data is not None:
+                fld_id = json_data.get("id", None)
+                fld_rpdo = json_data.get("rpdo", False)
+                fld_tpdo = json_data.get("tpdo", False)
+                fld_base_name = json_data.get("base", None)
             if isinstance(fld.type, Type): # or not isinstance(fld.type.typename.segments[0], NameSpecifier):
                 field_name = fld.name
                 field_type = fld.type.typename.segments[0].name
                 if field_type in typedefs:
                     field_type = typedefs[field_type]
-                field_comment = getComment(fld.doxygen)
+                field_comment = getComment(doxygen_comment)
                 v = Var(field_type, field_name)
                 if field_name.startswith(PARAM_PREFIX):
                     v.is_param = True
                 v.is_const = fld.type.const
                 v.comment = field_comment
+                v.id = fld_id
+                v.rpdo = fld_rpdo
+                v.tpdo = fld_tpdo
+                v.base_name = fld_base_name
                 structure.addField(v)
             elif isinstance(fld.type, Array):
                 #print(fld)
@@ -249,13 +300,17 @@ def collectData(header: str) -> list[Struct]:
                 arr_type = fld.type.array_of.typename.segments[0].name
                 if arr_type in typedefs:
                     arr_type = typedefs[arr_type]
-                arr_comment = getComment(fld.doxygen)
-                a = Var(arr_type, arr_name, arr_size)
+                arr_comment = getComment(doxygen_comment)
+                v = Var(arr_type, arr_name, arr_size)
                 if arr_name.startswith(PARAM_PREFIX):
-                    a.is_param = True
-                a.is_const = fld.type.array_of.const
-                a.comment = arr_comment
-                structure.addField(a)
+                    v.is_param = True
+                v.is_const = fld.type.array_of.const
+                v.comment = arr_comment
+                v.id = fld_id
+                v.rpdo = fld_rpdo
+                v.tpdo = fld_tpdo
+                v.base_name = fld_base_name
+                structure.addField(v)
         structs.append(structure)
     return structs
 
@@ -286,6 +341,26 @@ def getDefaultRegVar(name: str, subIndex: int) -> RegVar:
     return rv
 
 
+def getRegFlagsEFlags(fieldVar: Var) -> tuple[int, int]:
+    flags = 0
+    eflags = RegEFlag.CO_SDO_R
+
+    if fieldVar.is_param:
+        flags = flags | RegFlag.CONF
+
+    if fieldVar.is_const:
+        flags = flags | RegFlag.READONLY
+    else:
+        eflags = eflags | RegEFlag.CO_SDO_W
+
+    if fieldVar.rpdo:
+        eflags = eflags | RegEFlag.CO_RPDO
+    if fieldVar.tpdo:
+        eflags = eflags | RegEFlag.CO_TPDO
+
+    return flags, eflags
+
+
 def getRegVar(memPath: str, subIndex: int, fieldVar: Var, name: str or None, structs: list[Struct]) -> list[RegVar] or None:
     #print(subIndex, fieldVar)
     regVars: list[RegVar] = []
@@ -304,12 +379,7 @@ def getRegVar(memPath: str, subIndex: int, fieldVar: Var, name: str or None, str
             rv = getDefaultRegVar(varName, subIndex)
             rv.dataType = CType.toRegDataType(fieldVar.typename)
             rv.memAddr = f"{memPath}.{fieldVar.name}" # &
-            if fieldVar.is_param:
-                rv.flags = rv.flags | RegFlag.CONF
-            if fieldVar.is_const:
-                rv.flags = rv.flags | RegFlag.READONLY
-            else:
-                rv.eflags = rv.eflags | RegEFlag.CO_SDO_W
+            rv.flags, rv.eflags = getRegFlagsEFlags(fieldVar)
             rv.description = fieldVar.comment
             regVars.append(rv)
             #print(rv)
@@ -319,12 +389,7 @@ def getRegVar(memPath: str, subIndex: int, fieldVar: Var, name: str or None, str
                     rv = getDefaultRegVar(f"{varName}{i}", subIndex)
                     rv.dataType = CType.toRegDataType(fieldVar.typename)
                     rv.memAddr = f"{memPath}.{fieldVar.name}[{i}]" # &
-                    if fieldVar.is_param:
-                        rv.flags = rv.flags | RegFlag.CONF
-                    if fieldVar.is_const:
-                        rv.flags = rv.flags | RegFlag.READONLY
-                    else:
-                        rv.eflags = rv.eflags | RegEFlag.CO_SDO_W
+                    rv.flags, rv.eflags = getRegFlagsEFlags(fieldVar)
                     rv.description = fieldVar.comment
                     regVars.append(rv)
                     subIndex = subIndex + 1
@@ -334,12 +399,7 @@ def getRegVar(memPath: str, subIndex: int, fieldVar: Var, name: str or None, str
                 rv.dataType = DataType.MEM
                 rv.memAddr = f"{memPath}.{fieldVar.name}" # &
                 rv.count = DataType.getSize(CType.toRegDataType(fieldVar.typename)) * fieldVar.count
-                if fieldVar.is_param:
-                    rv.flags = rv.flags | RegFlag.CONF
-                if fieldVar.is_const:
-                    rv.flags = rv.flags | RegFlag.READONLY
-                else:
-                    rv.eflags = rv.eflags | RegEFlag.CO_SDO_W
+                rv.flags, rv.eflags = getRegFlagsEFlags(fieldVar)
                 rv.description = fieldVar.comment
                 regVars.append(rv)
                 # print(rv)
@@ -368,23 +428,63 @@ def getRegVar(memPath: str, subIndex: int, fieldVar: Var, name: str or None, str
 def getRegEntry(index: int, entryVar: Var, entryType: Struct, structs: list[Struct]) -> RegEntry:
     re = getDefaultRegEntry(entryVar.name, index)
     re.description = entryVar.comment
-    cntVar = getDefaultRegVar("count", 0)
+    cntVar: RegVar = getDefaultRegVar("count", 0)
     cntVar.dataType = DataType.U8
     cntVar.flags = cntVar.flags | RegFlag.READONLY
     cntVar.eflags = cntVar.eflags | RegEFlag.CO_COUNT
     cntVar.description = "Number of sub entries count"
     re.addVar(cntVar)
 
+    # reg vars dict
+    regVars: dict[int, RegVar] = dict()
+
+    # set of predefined ids
+    predef_ids = set()
+    predef_ids.add(cntVar.subIndex)
+    # collect predefined ids
+    for field in entryType.fields:
+        if field.id is not None:
+            # if the id already exists
+            if field.id in predef_ids:
+                print("Ignoring duplicate predefined id in subentry", field.name)
+                # remove it
+                field.id = None
+            else:
+                predef_ids.add(field.id)
+
     subIndex = 1
     for field in entryType.fields:
-        rvs = getRegVar(re.name, subIndex, field, None, structs)
+        # predefined ids
+        id = field.id
+        if id is None:
+            while subIndex in predef_ids:
+                subIndex = subIndex + 1
+            id = subIndex
+        else:
+            if id in regVars:
+                rv_collis: Var = regVars.get(id)
+                print("subindex", id, "collision:", re.name, field.name, "with", None if not rv_collis else rv_collis.name)
+                id = id + 1
+
+        rvs = getRegVar(re.name, id, field, None, structs)
         if rvs:
             for rv in rvs:
-                # print(rv)
-                re.addVar(rv)
-            subIndex = subIndex + len(rvs)
+                #print(rv)
+                # correct id if subindex exists
+                id = rv.subIndex
+                while id in regVars:
+                    id = id + 1
+                rv.subIndex = id
+                regVars[rv.subIndex] = rv
+            if field.id is None:
+                subIndex = id + 1
 
-    cntVar.defValue = str(0 if subIndex == 0 else subIndex - 1)
+    regVarsList = list(dict(sorted(regVars.items())).values())
+    for rv in regVarsList:
+        re.addVar(rv)
+
+    #print(re.regVars)
+    cntVar.defValue = str(len(regVarsList))
     return re
 
 
@@ -426,6 +526,23 @@ def exportRegEntries(fileName: str, regentries: list[RegEntry]):
         return
 
 
+def getBase(base_name: str) -> tuple[str, str] or None:
+    dot_pos: int = base_name.find(".")
+    if dot_pos < 0:
+        return None
+
+    mod_name = base_name[0:dot_pos]
+    if not mod_name:
+        return None
+
+    field_name = base_name[dot_pos+1:].replace(".", "_")
+    if not field_name:
+        return None
+
+    return mod_name, field_name
+
+
+
 def parseModules():
     parse_data = parse_file(os.path.join(PROJECT_PATH, "modules", "modules.h"), encoding=ENCODING,
                             options=makeParseOpts())
@@ -433,16 +550,39 @@ def parseModules():
     incs: list[str] = []
     mods: list[Var] = []
     structs: list = []
-    regentries: list[RegEntry] = []
+    regentries_by_id: dict[int, RegEntry] = {}
+    regentries_by_name: dict[str, RegEntry] = {}
 
     incs = getHeaders(PROJECT_PATH, EXCLUDE_DIRS)
     mods = getVars(parse_data)
+    max_count = None # -1
     for inc in incs:
+        if max_count is not None and max_count >= 0:
+            if max_count != 0:
+                max_count = max_count - 1
+            else:
+                break
         structs = structs + collectData(inc)
     # for inc in ["data_log\\data_log.h", "sys_main\\sys_main.h", "fsm\\fsm.h"]: #
     #     structs = structs + collectData(inc)
 
     #print(structs)
+
+    # set of predefined ids
+    predef_ids = set()
+    # collect predefined ids
+    for m in mods:
+        if m.id is not None:
+            # if the id already exists
+            if m.id in predef_ids:
+                print("Ignoring duplicate predefined id in module", m.name)
+                # remove it
+                m.id = None
+            else:
+                predef_ids.add(m.id)
+
+    # for m in mods:
+    #     print(m.name, m.id)
 
     index = BEGIN_INDEX
 
@@ -450,11 +590,78 @@ def parseModules():
         ms: Struct = getVarStruct(m.typename, structs)
         if not ms:
             continue
-        re = getRegEntry(index, m, ms, structs)
-        regentries.append(re)
+        # predefined ids
+        id = m.id
+        if id is None:
+            while index in predef_ids:
+                index = index + STEP_INDEX
+            id = index
+        re = getRegEntry(id, m, ms, structs)
+        regentries_by_id[id] = re
+        regentries_by_name[re.name] = re
         index = index + STEP_INDEX
 
+
+    # find base id's
+    for m in mods:
+        ms: Struct = getVarStruct(m.typename, structs)
+        if not ms:
+            continue
+        m_re = regentries_by_name.get(m.name, None)
+        if not m_re:
+            print("Can't find module reg entry:", m.name)
+            continue
+        for fld in ms.fields:
+            if fld.base_name:
+                base_mod, base_fld = getBase(fld.base_name)
+                if not base_mod or not base_fld:
+                    print("Error in base name", fld.base_name)
+                    continue
+
+                base_re: RegEntry = regentries_by_name.get(base_mod, None)
+                if not base_re:
+                    print("Can't find base reg entry:", base_mod)
+                    continue
+
+                if not base_re.regVars:
+                    continue
+
+                base_re_fld: RegVar = None
+                for re_fld in base_re.regVars:
+                    #print(re_fld)
+                    if fld.base_name == re_fld.memAddr or base_fld == re_fld.name:
+                        base_re_fld = re_fld
+                        break
+
+                if not base_re_fld:
+                    print("Can't find base field", base_fld, "in reg entry", base_re.name)
+                    continue
+
+                #full_id = (base_re.index << 8) | (base_re_fld.subIndex & 0xff)
+
+                m_re_fld = None
+                m_fld_mem = '.'.join([m_re.name, fld.name])
+                for rv in m_re.regVars:
+                    if rv.memAddr == m_fld_mem:
+                        m_re_fld = rv
+                        break
+
+                if not m_re_fld:
+                    print("Can't find module field", m_fld_mem)
+                    continue
+
+                m_re_fld.baseIndex = base_re.index
+                m_re_fld.baseSubIndex = base_re_fld.subIndex
+                #print("Found base", fld.base_name, ":", base_re.name, base_re_fld.name, full_id)
+                #print(fld.name, fld.base_name, base_mod, base_fld)
+
+    regentries = list(dict(sorted(regentries_by_id.items())).values())
+    regentries_by_id.clear()
+    regentries_by_name.clear()
+
     #print(regentries)
+    #return
+
     # for re in regentries:
     #     print(re)
     #     for rv in re.regVars:
